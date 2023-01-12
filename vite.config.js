9323620837ef6aac7533e6931a1b22b6ac3e4f86
zhangzhuo <1114976368@qq.com>
2023-01-12 23:52:56 +0800

    feat: 添加vitest配置

--- vite.config.js
+++ vite.config.js
@@ -27,4 +27,15 @@ export default defineConfig({
       formats: ["esm", "umd","iife"],
     },
   },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  }
 });
