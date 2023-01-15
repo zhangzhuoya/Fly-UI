e8ac80004009d0752d0f25dc1f9bcfb998ab99e2
zhangzhuo <1114976368@qq.com>
2023-01-15 12:28:27 +0800

    feat: switch组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <FlySwitch :vlaue="true"></FlySwitch>
+  <FlySwitch v-model:value="value" :disabled="true"></FlySwitch>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import FlySwitch from "./lib/switch/FlySwitch.vue";
 export default {
   name: "App",
   components: {
     FlySwitch,
+  },
+  setup() {
+    const value = ref(true);
+    return {
+      value,
+    };
   },
 };
 </script>
--- src/lib/switch/FlySwitch.vue
+++ src/lib/switch/FlySwitch.vue
@@ -1,29 +1,78 @@
 <template>
-  <button><span>12124</span></button>
-  <!-- <div class="switch" >switch1{{ value }}</div> -->
+  <button class="fly-switch" :class="{'fly-checked': value,'disabled': disabled}" @click="changeSwitch"><span></span></button>
 </template>
 
 <script lang="ts">
-// import ref from "vue";
 export default {
   name: 'FLySwitch',
   props: {
-    value: {
-      type: Boolean,
-      default: true
+    value:{
+      type: Boolean
+    },
+    disabled: {
+      type: Boolean
     }
   },
   setup(props, context) {
-    console.log(props, context);
+    const changeSwitch =() =>{
+      if (!props.disabled) {
+        context.emit('update:value', !props.value)
+      }
+    }
+    return {
+      changeSwitch
+    }
   },
 };
 </script>
 
-<style lang="less">
-button {
-  color: red;
-  span {
-    color: green;
+<style lang="scss">
+@use "sass:math";
+$h: 22px;
+$h2: $h - 4px;
+.fly-switch {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: math.div($h, 2);
+  position: relative;
+  &.disabled {
+        cursor: not-allowed;
+
+        &:hover {
+            cursor: not-allowed;
+        }
+    }
+  >span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: math.div($h2, 2);
+    transition: all 250ms;
+  }
+  &.fly-checked {
+    background: #1890ff;
+    >span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    >span {
+      width: $h2 + 4px;
+    }
+  }
+  &.fly-checked:active {
+    >span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
 }
 </style>
