efb94f34151c15588d9a05afbfd0bca8e2872a01
zhangzhuo <zhangzhuo@zhangzhuodeMacBook-Pro.local>
2023-01-18 23:10:08 +0800

    feat: 创建dialog

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,20 @@
   <FlyButton theme="danger">danger</FlyButton>
   <FlyButton theme="text">text</FlyButton>
   <FlyButton theme="text" :isLoading="true">text</FlyButton>
+  <FlyDialog></FlyDialog>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 import FlySwitch from "./lib/switch/FlySwitch.vue";
 import FlyButton from "./lib/button/FlyButton.vue";
+import FlyDialog from "./lib/dialog/FlyDialog.vue";
 export default {
   name: "App",
   components: {
     FlySwitch,
     FlyButton,
+    FlyDialog,
   },
   setup() {
     const value = ref(true);
---
+++ src/lib/dialog/FlyDialog.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div class="fly-dialog">
+    <div class="fly-dialog-overlay">
+
+    </div>
+    <div class="fly-dailog-wrapper">
+        <header>
+            标题
+        </header>
+        <main>
+            内容
+        </main>
+        <footer>
+            <FlyButton>cancel</FlyButton>
+            <FlyButton>ok</FlyButton>
+        </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import FlyButton from '../button/FlyButton.vue'
+export default {
+    name:'FlyDialog',
+    components: {
+        FlyButton
+    }
+
+}
+</script>
+
+<style lang="scss">
+.fly-dialog {
+    width: 500px;
+}
+
+</style>
