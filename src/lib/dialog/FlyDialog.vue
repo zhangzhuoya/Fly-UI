a941e0d0ed5e9bdeb5e8aa97603ce05cc9aae5ec
zhangzhuo <1114976368@qq.com>
2023-01-18 23:26:30 +0800

    feat: dialog基本样式

--- src/lib/dialog/FlyDialog.vue
+++ src/lib/dialog/FlyDialog.vue
@@ -1,38 +1,91 @@
-
 <template>
-  <div class="fly-dialog">
-    <div class="fly-dialog-overlay">
-
-    </div>
-    <div class="fly-dailog-wrapper">
-        <header>
-            标题
-        </header>
+    <div class="fly-dialog-overlay"></div>
+    <div class="fly-dialog-wrapper">
+        <div class="fly-dialog">
+        <header>标题 <span class="fly-dialog-close"></span></header>
         <main>
-            内容
+            <p>第一行字</p>
+            <p>第二行字</p>
         </main>
         <footer>
-            <FlyButton>cancel</FlyButton>
-            <FlyButton>ok</FlyButton>
+            <Button level="main">OK</Button>
+            <Button>Cancel</Button>
         </footer>
+        </div>
     </div>
-  </div>
 </template>
-
 <script lang="ts">
-import FlyButton from '../button/FlyButton.vue'
+import Button from "../button/FlyButton.vue";
 export default {
-    name:'FlyDialog',
     components: {
-        FlyButton
-    }
-
-}
+    Button,
+    },
+};
 </script>
 
 <style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
 .fly-dialog {
-    width: 500px;
+    background: white;
+    border-radius: $radius;
+    box-shadow: 0 0 3px fade_out(black, 0.5);
+    min-width: 15em;
+    max-width: 90%;
+    &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+    }
+    &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+    }
+    >header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+    }
+    >main {
+    padding: 12px 16px;
+    }
+    >footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+    }
+    &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+        content: '';
+        position: absolute;
+        height: 1px;
+        background: black;
+        width: 100%;
+        top: 50%;
+        left: 50%;
+    }
+    &::before {
+        transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+        transform: translate(-50%, -50%) rotate(45deg);
+    }
+    }
 }
-
 </style>
